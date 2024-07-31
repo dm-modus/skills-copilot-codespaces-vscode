c66afa03d08d0d3062fe4f271c15de0193b6b5dc
dm-modus <dmitry.mazur@moduscreate.com>
2024-07-31 18:48:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = {
+        name: 'John',
+        age: 25,
+        skills: ['js', 'html', 'css'],
+        salary: 1000
+    };
+
+    return member;
+}
